95547f5ff9412bdba3c3121973574ccb55c84c14
Drac Zihper <draczihper@gmail.com>
2024-03-15 12:31:31 +0300

    Add some books for styling

--- script.js
+++ script.js
@@ -20,4 +20,22 @@ tableEl.innerHTML += `<tr>
 <td>J.R.R. Tolkien</td>
 <td>295 pages</td>
 <td>not read yet</td>
+</tr>
+<tr>
+<td>The Power of your subconcious mind</td>
+<td>J. Murphy</td>
+<td>222 pages</td>
+<td>read</td>
+</tr>
+<tr>
+<td>Think and Grow rich</td>
+<td>John Doe</td>
+<td>183 pages</td>
+<td>not read yet</td>
+</tr>
+<tr>
+<td>The physchology of money</td>
+<td>M. Housel</td>
+<td>188</td>
+<td>read</td>
 </tr>`
